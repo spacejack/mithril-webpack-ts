289334b3e912c14e74fc991f5a7f8271322e4d1f
spacejack <spacejack@gmail.com>
2017-05-07 14:45:06 -0400

    Cleanup

--- scripts/postcss.config.js
+++ scripts/postcss.config.js
@@ -5,8 +5,5 @@ module.exports = {
 	plugins: [
 		PostcssNesting(),
 		Autoprefixer()
-		//require('postcss-smart-import')({ /* ...options */ }),
-		//require('precss')({ /* ...options */ }),
-		//require('autoprefixer')({ /* ...options */ })
 	]
 }
