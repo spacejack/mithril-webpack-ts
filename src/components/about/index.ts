da94d94b9b09c139eb4ccfa1a0d58221f946edd8
spacejack <spacejack@gmail.com>
2017-05-07 16:40:37 -0400

    Use MS tslint as base rules

--- src/components/about/index.ts
+++ src/components/about/index.ts
@@ -6,8 +6,8 @@ export default {
 	view (vnode) {
 		return m('.page.about', [
 			m(nav),
-			m('h1', "About"),
-			m('p', "This is the about page. Stuff about the app.")
+			m('h1', 'About'),
+			m('p', 'This is the about page. Stuff about the app.')
 		])
 	}
-} as m.Component<{},{}>
+} as m.Component<{}, {}>
